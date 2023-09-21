7fed8fb567defa52d9fc47fd42b3be5b224cb43b
gitadityakumar <ITSADITYASINGH05@GMAIL.COM>
2023-09-22 00:06:56 +0530

    first que in leetcode easy

---
+++ src/L3q22.java
@@ -0,0 +1,31 @@
+public class L3q22 {
+    public static void main(String[] args) {
+        Solution solution = new Solution(); // Create an instance of the Solution class
+
+        // Test your method with specific input values
+        int result1 = solution.subtractProductAndSum(234);
+        int result2 = solution.subtractProductAndSum(4421);
+        int result3 = solution.subtractProductAndSum(12345);
+
+        // Print the results
+        System.out.println("Result 1: " + result1);
+        System.out.println("Result 2: " + result2);
+        System.out.println("Result 3: " + result3);
+    }
+}
+
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            product *= digit;
+            sum += digit;
+            n /= 10;
+        }
+
+        return product - sum;
+    }
+}
