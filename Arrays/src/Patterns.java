88ae08e4eef2bfbce007e183a3c952864d3e887b
gitadityakumar <itsadityasingh05@gmail.com>
2024-02-28 23:31:52 +0530

    more question

--- Arrays/src/Patterns.java
+++ Arrays/src/Patterns.java
@@ -12,7 +12,8 @@ public class Patterns {
                 squareP(n);
         */
 //        invertedRightPyramid(n);
-        invertedNumberedRightPyramid(n);
+//        invertedNumberedRightPyramid(n);
+        starPattern(n);
 
 
 
@@ -75,5 +76,32 @@ public class Patterns {
 
         }
         
+    }
+    static void starPattern(int num){
+        for (int i = 0; i < num; i++){
+
+            // For printing the spaces before stars in each row
+            for (int j =0; j<num-i-1; j++)
+            {
+                System.out.print(" ");
+            }
+
+            // For printing the stars in each row
+            for(int j=0;j< 2*i+1;j++){
+
+                System.out.print("*");
+            }
+
+            // For printing the spaces after the stars in each row
+            for (int j =0; j<num-i-1; j++)
+            {
+                System.out.print(" ");
+            }
+
+            // As soon as the stars for each iteration are printed, we move to the
+            // next row and give a line break otherwise all stars
+            // would get printed in 1 line.
+            System.out.println();
+        }
     }
 }
