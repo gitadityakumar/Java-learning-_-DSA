525f4f8528a1eb617359e459503d6b6549f329d4
gitadityakumar <itsadityasingh05@gmail.com>
2024-02-29 23:37:50 +0530

    isp

--- Arrays/src/Patterns.java
+++ Arrays/src/Patterns.java
@@ -13,7 +13,8 @@ public class Patterns {
         */
 //        invertedRightPyramid(n);
 //        invertedNumberedRightPyramid(n);
-        starPattern(n);
+//        starPattern(n);
+        invertedStarPattern(n);
 
 
 
@@ -101,6 +102,29 @@ public class Patterns {
             // As soon as the stars for each iteration are printed, we move to the
             // next row and give a line break otherwise all stars
             // would get printed in 1 line.
+            System.out.println();
+        }
+    }
+
+    static void invertedStarPattern(int num){
+        for (int i = 0; i < num; i++)
+        {
+            // For printing the spaces before stars in each row
+            for (int j =0; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            // For printing the stars in each row
+            for(int j=0;j< 2*num -(2*i +1);j++){
+
+                System.out.print("*");
+            }
+            for (int j =0; j<i; j++)
+            {
+                System.out.print(" ");
+            }
+
             System.out.println();
         }
     }
